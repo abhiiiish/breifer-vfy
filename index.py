ddbba24ed68360518bf27c92541d1c181b83f47b
abhiiiesh <130227928+abhiiiesh@users.noreply.github.com>
2023-05-13 17:37:23 +0530

    Update index.py

--- index.py
+++ index.py
@@ -27,11 +27,18 @@ def home():
         establishment = request.form['establishment']
         services = request.form['services']
         location = request.form['location']
-        prompt = f"Generate 5 grammatically correct with only 150 characters description for business, Business name {company_name} and Establish year as {establishment} and Keyword for business are {services}, located in {location},."
-        description = generate_description(prompt)
-        return render_template('index.html', description=description)
+        prompt = f"Generate 5 grammatically correct with only 150 characters description for business, Business name {company_name} and Establish year as {establishment}  and services by  business are {services} , located in {location}."
+
+        # Generate 5 descriptions
+        descriptions = []
+        for _ in range(5):
+            description = generate_description(prompt)
+            descriptions.append(description)
+
+        return render_template('index.html', descriptions=descriptions)
     else:
         return render_template('index.html')
+
     
 
 if __name__ == '__main__':
